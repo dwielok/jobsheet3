1d14f7ee1b423c4d61380411ceac0519d226d1f6
dwielok <dwieloknuraini773@gmail.com>
2022-03-06 14:45:45 +0700

    router, controller, view about

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 //     return view('index');
 // });
 Route::get('/', [PageController::class, 'index']);
+
+Route::get('/about', [PageController::class, 'about']);
