b524475790556da5dc7de37c2e26ac96b55661c3
dwielok <dwieloknuraini773@gmail.com>
2022-03-06 15:10:41 +0700

    router, controller, view testmonial

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -15,4 +15,9 @@ class PageController extends Controller
     {
         return view('about');
     }
+
+    public function testmonial()
+    {
+        return view('testmonial');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PageController::class, 'index']);
 
 Route::get('/about', [PageController::class, 'about']);
+
+Route::get('/testmonial', [PageController::class, 'testmonial']);
